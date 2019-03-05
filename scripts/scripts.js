310f470829301eebec6b44cacc77df6a793644aa
ss2sc <ss2sc@virginia.edu>
2019-03-05 18:05:31 -0500

    Centered views, no close button yet

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,16 @@
 function openGame() {
-    document.getElementById("home").display = "none";
-    //document.getElementById("play").display = "block";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("play").style.display = "block";
+}
+
+function openInstructions() {
+    document.getElementById("leaderboard").style.display = "none";
+    document.getElementById("centerscreen").style.display = "block";
+    document.getElementById("howtoplay").style.display = "block";
+}
+
+function openLeaderboard() {
+    document.getElementById("howtoplay").style.display = "none";
+    document.getElementById("centerscreen").style.display = "block";
+    document.getElementById("leaderboard").style.display = "block";
 }
