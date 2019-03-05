3b028ffecbee4f9e14b31d0efadf7e59491c4278
Vivien Chen <vc2cw@virginia.edu>
2019-03-05 16:41:08 -0500

    Added hamburger menu for mobile

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,13 @@ function openGame() {
     document.getElementById("home").display = "none";
     //document.getElementById("play").display = "block";
 }
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
